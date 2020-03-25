a428339113c973be443766bf2ae47ca9fc50fd99
Daniel Lwetabe <acehanks@gmail.com>
2020-03-25 08:48:57 +0300

    declare .env variables

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
-t.go
+package main
+
+import (
+	"os"	
+)
+
+func main() {
+	a := App {}
+	a.Initialize(
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME")
+	)
+}
+
+a.Run(":8010")
